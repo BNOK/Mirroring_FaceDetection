609a4d1eabe270196f8302d92afa8b1ba7bdc5bc
BNOK <52835202+BNOK@users.noreply.github.com>
2021-06-06 11:24:12 +0100

    Update

--- FaceDetection/Assets/Scripts/ShapeMover.cs
+++ FaceDetection/Assets/Scripts/ShapeMover.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ShapeMover : MonoBehaviour
 {
+    public Vector3 pos;
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +19,16 @@ public class ShapeMover : MonoBehaviour
     {
         if (FaceDetector.NormalizedFacePositions.Count == 0)
             return;
+        pos.x = FaceDetector.NormalizedFacePositions[0].x * xSpeed;
+        pos.y = FaceDetector.NormalizedFacePositions[0].y * ySpeed;
+        pos.z = FaceDetector.NormalizedFacePositions[0].z * zSpeed;
 
-        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(
-            FaceDetector.NormalizedFacePositions[0].x, 
-            FaceDetector.NormalizedFacePositions[0].y, 
-            FaceDetector.NormalizedFacePositions[0].z));
+        transform.position = pos;
+        //transform.position = Camera.main.ViewportToWorldPoint(new Vector3(
+        //    FaceDetector.NormalizedFacePositions[0].x, 
+        //    FaceDetector.NormalizedFacePositions[0].y, 
+        //    FaceDetector.NormalizedFacePositions[0].z));
+
+        
     }
 }
